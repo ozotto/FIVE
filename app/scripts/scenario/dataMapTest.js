df7dea1dc5219bfaf683c6acc06fbbc2d32745b9
ozotto <ozotto@gmail.com>
2015-05-07 17:06:47 +0200

    timeSuisse

---
+++ app/scripts/scenario/dataMapTest.js
@@ -0,0 +1,38 @@
+/**
+ * Created by Oscar on 07.05.15.
+ */
+var mapData;
+mapData = [
+    {   "id": 1, "name": "Bulle", date: new Date(2014, 1, 10), "coordinates": [7.063768, 46.613474] },
+    {   "id": 2, "name": "Romont", date: new Date(2014, 4, 5), "coordinates": [6.914822, 46.691155] },
+    {   "id": 3, "name": "Grand Place", date: new Date(2015, 4, 5), "coordinates": [7.155774, 46.802842], "description":"Allée des Grand-Places" },
+    {   "id": 4, "name": "Rue de la Sarine 4", date: new Date(2015, 4, 5), "coordinates": [7.152512,46.803047 ] },
+    {   "id": 5, "name": "Fribourg Centre", date: new Date(2015, 4, 5), "coordinates": [7.158113, 46.803620] },
+    {   "id": 6, "name": "Rue Louis-d'Affry 4", date: new Date(2015, 4, 5), "coordinates": [7.150366, 46.803562] },
+    {   "id": 7, "name": "Rue du Pont-Suspendu 18", date: new Date(2015, 4, 5), "coordinates": [7.163735, 46.805853] },
+    {   "id": 8, "name": "Passerelle du Grand-Pont 1", date: new Date(2016, 4, 5), "coordinates": [6.630608, 46.520723] },
+    {   "id": 9, "name": "Eglise Evangélique de la Grâce", date: new Date(2016, 4, 5), "coordinates": [6.625973, 46.523263] },
+    {   "id": 10, "name": "Avenue de Provence 16", date: new Date(2016, 4, 5), "coordinates": [6.608035,46.523263 ] },
+    {   "id": 11, "name": "5 98-106 Lausanne", date: new Date(2016, 4, 5), "coordinates": [6.613270, 46.530526] },
+    {   "id": 12, "name": "CHUV", date: new Date(2015, 4, 5), "coordinates": [6.641509, 46.526452] },
+    {   "id": 13, "name": "Golf Club de Lausanne", date: new Date(2016, 4, 5), "coordinates": [6.673609,46.562816 ] },
+    {   "id": 14, "name": "Sottens", date: new Date(2016, 4, 5), "coordinates": [6.739699, 46.656449] },
+    {   "id": 15, "name": "Moudon", date: new Date(2016, 4, 5), "coordinates": [6.803900, 46.669879] },
+    {   "id": 16, "name": "Place de Cornavin 3", date: new Date(2016, 4, 5), "coordinates": [6.142415, 46.209989] },
+    {   "id": 17, "name": "Sécheron", date: new Date(2016, 4, 5), "coordinates": [6.144475, 46.222342] },
+    {   "id": 18, "name": "Banque cantonale", date: new Date(2016, 4, 5), "coordinates": [6.133746, 46.211771] },
+    {   "id": 19, "name": "Avenue Ernest-Pictet 2", date: new Date(2015, 4, 5), "coordinates": [6.127480, 46.211445] },
+    {   "id": 20, "name": "Iglesia de Dios Ministerial de Jesucristo Internacional", date: new Date(2015, 4, 5), "coordinates": [6.127309,46.207851 ] },
+    {   "id": 21, "name": "Boulevard de Saint-Georges 4", date: new Date(2015, 4, 5), "coordinates": [6.130956, 46.201257] },
+    {   "id": 22, "name": "Worb", date: new Date(2015, 4, 5), "coordinates": [7.542876, 46.935915] },
+    {   "id": 23, "name": "Avenue d'Aïre 69", date: new Date(2015, 4, 5), "coordinates": [6.122459,46.206396 ] },
+    {   "id": 24, "name": "Chemin des Sports 2-4", date: new Date(2015, 4, 5), "coordinates": [6.117996,46.206277 ] },
+    {   "id": 25, "name": "hepia", date: new Date(2015, 4, 5), "coordinates": [6.114048,46.205445 ] },
+    {   "id": 26, "name": "Rue de la Prairie 4", date: new Date(2015, 4, 5), "coordinates": [6.135162, 46.209514] },
+    {   "id": 27, "name": "Route de Meyrin 24", date: new Date(2015, 4, 5), "coordinates": [6.125377, 46.215038] },
+    {   "id": 28, "name": "Avenue du Bouchet 29", date: new Date(2015, 4, 5), "coordinates": [6.121086, 46.218185] },
+    {   "id": 29, "name": "Zürichbergstrasse 196", date: new Date(2016, 4, 5), "coordinates": [8.571691,47.381611 ] },
+    {   "id": 30, "name": "Lavertezzo", date: new Date(2015, 4, 5), "coordinates": [8.893006,46.240008 ] },
+    {   "id": 30, "name": "Riehenstrasse 328", date: new Date(2015, 4, 5), "coordinates": [7.616776,47.570311 ] }
+];
+
